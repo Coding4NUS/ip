7f3bd4495fa3c5f2b3fd92c44d56afb94f1c3aeb
Lucas Wong <e1398095@u.nus.edu>
2026-01-21 16:29:02 +0800

    Level-1 Echo

--- src/main/java/NUSGPT.java
+++ src/main/java/NUSGPT.java
@@ -1,14 +1,35 @@
+import java.util.Scanner;
+
 public class NUSGPT {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         // horizontal line template
         String horizontal_line = "____________________________________________________________\n";
 
-        // print skeleton version
+        // print greeting
         System.out.println(horizontal_line
                 + " Hello! I'm NUSGPT\n"
                 + " What can I do for you?\n"
-                + horizontal_line
+                + horizontal_line);
+
+        // detect user commands
+        String command = scanner.nextLine();
+
+        // if the command is not bye echo
+        while (!command.equals("bye")) {
+            System.out.println(horizontal_line
+                    + " " + command + "\n"
+                    + horizontal_line);
+
+            // let the user input a new command
+            command = scanner.nextLine();
+        }
+
+        // if the command is bye exit
+        System.out.println(horizontal_line
                 + " Bye. Hope to see you again soon!\n"
                 + horizontal_line);
+        scanner.close();
     }
 }
