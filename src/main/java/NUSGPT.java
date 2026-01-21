3dfbf709a8e0f47c67728fd72579834ceae2f3ed
Lucas Wong <e1398095@u.nus.edu>
2026-01-21 17:07:01 +0800

    Level-2 Add, List

--- src/main/java/NUSGPT.java
+++ src/main/java/NUSGPT.java
@@ -4,6 +4,11 @@ public class NUSGPT {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // store up to 100 tasks
+        int taskNumber = 0;
+        int maxTasks = 100;
+        String[] tasks = new String[maxTasks];
+
         // horizontal line template
         String horizontal_line = "____________________________________________________________\n";
 
@@ -16,12 +21,26 @@ public class NUSGPT {
         // detect user commands
         String command = scanner.nextLine();
 
-        // if the command is not bye echo
+        // check if the command is not bye
         while (!command.equals("bye")) {
-            System.out.println(horizontal_line
-                    + " " + command + "\n"
-                    + horizontal_line);
-
+            // if the command is list show all the items in the list
+            if (command.equals("list")) {
+                System.out.println(horizontal_line);
+                // for each item in the list print it in order
+                for (int i = 0; i < taskNumber; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(horizontal_line);
+            } else {
+                // store the text in the list if it has space
+                if (taskNumber < maxTasks) {
+                    tasks[taskNumber] = command;
+                    taskNumber++;
+                    System.out.println(horizontal_line
+                            + "added: " + command + "\n"
+                            + horizontal_line);
+                }
+            }
             // let the user input a new command
             command = scanner.nextLine();
         }
