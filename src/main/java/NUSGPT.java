efc2ebd67c1380dafe3c0fe8f3f29e4a7d38c591
Lucas Wong <e1398095@u.nus.edu>
2026-01-21 20:31:12 +0800

    Level-3 Mark as Done

--- src/main/java/NUSGPT.java
+++ src/main/java/NUSGPT.java
@@ -7,7 +7,7 @@ public class NUSGPT {
         // store up to 100 tasks
         int taskNumber = 0;
         int maxTasks = 100;
-        String[] tasks = new String[maxTasks];
+        Task[] tasks = new Task[maxTasks];
 
         // horizontal line template
         String horizontal_line = "____________________________________________________________\n";
@@ -26,25 +26,61 @@ public class NUSGPT {
             // if the command is list show all the items in the list
             if (command.equals("list")) {
                 System.out.println(horizontal_line);
+                System.out.println("Here are the tasks in your list:");
                 // for each item in the list print it in order
                 for (int i = 0; i < taskNumber; i++) {
                     System.out.println((i + 1) + ". " + tasks[i]);
                 }
                 System.out.println(horizontal_line);
+            // check if the command says to mark an item on the list
+            } else if (command.startsWith("mark")) {
+                // get the index of the item in the list to be marked
+                int index = Integer.parseInt(command.substring(5));
+                // check if the index is a valid number
+                if (index < 1 || index > taskNumber) {
+                    System.out.println(horizontal_line
+                            + index + " is not a valid index\n"
+                            + horizontal_line);
+                } else {
+                    // mark the item in the list index
+                    Task task = tasks[index - 1];
+                    task.markDone();
+                    System.out.println(horizontal_line
+                            + "Nice! I've marked this task as done:\n"
+                            + task + "\n"
+                            + horizontal_line);
+                }
+            // check if the command says to unmark an item on the list
+            } else if (command.startsWith("unmark")) {
+                // get the index of the item in the list to be unmarked
+                int index = Integer.parseInt(command.substring(7));
+                // check if the index is a valid number
+                if (index < 1 || index > taskNumber) {
+                    System.out.println(horizontal_line
+                            + index + " is not a valid index\n"
+                            + horizontal_line);
+                } else {
+                    // unmark the item in the list index
+                    Task task = tasks[index - 1];
+                    task.markNotDone();
+                    System.out.println(horizontal_line
+                            + "OK, I've marked this task as not done yet:\n"
+                            + task + "\n"
+                            + horizontal_line);
+                }
             } else {
                 // store the text in the list if it has space
                 if (taskNumber < maxTasks) {
-                    tasks[taskNumber] = command;
-                    taskNumber++;
+                    tasks[taskNumber] = new Task(command);
                     System.out.println(horizontal_line
-                            + "added: " + command + "\n"
+                            + "added: " + tasks[taskNumber] + "\n"
                             + horizontal_line);
+                    taskNumber++;
                 }
             }
             // let the user input a new command
             command = scanner.nextLine();
         }
-
         // if the command is bye exit
         System.out.println(horizontal_line
                 + " Bye. Hope to see you again soon!\n"
---
+++ src/main/java/Task.java
@@ -0,0 +1,34 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void markNotDone() {
+        this.isDone = false;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
+}
